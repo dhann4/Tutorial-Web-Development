a1229cd0b52e66f6b850e7a85533c05638e134bc
Zero Zero Zero Zero <78632939+dhann4@users.noreply.github.com>
2021-02-28 16:46:20 +0800

    Add files via upload

---
+++ 03. JavaScript/00. Latihan/angkot1.js
@@ -0,0 +1,10 @@
+var jmlAngkot = 10;
+var angkotBeroperasi = 6;
+var noAngkot = 1;
+
+while(noAngkot <= jmlAngkot) {
+	
+	console.log('Angkot No. ' + noAngkot + ' beroperasi dengan baik.');
+
+noAngkot++;	
+}
---
+++ 03. JavaScript/00. Latihan/latihan1.js
@@ -0,0 +1,6 @@
+function jumlahVolumeDuaKubus(a, b) {
+
+	return a * a * a + b * b * b;
+}
+
+alert('Jumlah kubus 1 adalah : ' + jumlahVolumeDuaKubus(8, 3));
